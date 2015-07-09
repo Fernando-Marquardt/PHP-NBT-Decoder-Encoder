ad4127ed1b88ad8617f89493d4f86217a7ef16ba
Fernando Marquardt <fernando@magnumweb.com.br>
2015-07-09 18:45:46 -0300

    Test file changed to use PHPUnit e added configuration for Travis CI.

---
+++ tests/NBTTest.php
@@ -0,0 +1,82 @@
+<?php
+class NBTTest extends PHPUnit_Framework_TestCase {
+
+    protected $nbt;
+
+    public static function setUpBeforeClass() {
+        require 'nbt.class.php';
+    }
+
+    protected function setUp() {
+        $this->nbt = new NBT();
+        //$this->nbt->verbose = true;
+    }
+
+    public function testLoadFile() {
+        $this->nbt->loadFile('smalltest.nbt');
+
+        $this->assertNotEmpty($this->nbt->root);
+    }
+
+    public function testNBTTypes() {
+        $this->nbt->loadFile('bigtest.nbt');
+
+        print_r($this->nbt->root[0]);
+
+        $root = $this->nbt->root[0];
+        $values = $root['value'];
+
+        // Long
+        $this->assertEquals(4, $values[0]['type']);
+        $this->assertEquals('longTest', $values[0]['name']);
+        $this->assertEquals('9223372032559808511', $values[0]['value']);
+
+        // Short
+        $this->assertEquals(2, $values[1]['type']);
+        $this->assertEquals('shortTest', $values[1]['name']);
+        $this->assertEquals(32767, $values[1]['value']);
+
+        // String
+        $this->assertEquals(8, $values[2]['type']);
+        $this->assertEquals('stringTest', $values[2]['name']);
+        $this->assertEquals('HELLO WORLD THIS IS A TEST STRING ' . chr(197) . chr(196) . chr(214) .'!', $values[2]['value']);
+
+        // Float
+        $this->assertEquals(5, $values[3]['type']);
+        $this->assertEquals('floatTest', $values[3]['name']);
+        $this->assertEquals(0.49823147058487, $values[3]['value']);
+
+        // Integer
+        $this->assertEquals(3, $values[4]['type']);
+        $this->assertEquals('intTest', $values[4]['name']);
+        $this->assertEquals(2147483647, $values[4]['value']);
+
+        // Compound
+        $this->assertEquals(10, $values[5]['type']);
+        $this->assertEquals('nested compound test', $values[5]['name']);
+        $this->assertCount(2, $values[5]['value'][1]['value']);
+
+        // Byte
+        $this->assertEquals(1, $values[8]['type']);
+        $this->assertEquals('byteTest', $values[8]['name']);
+        $this->assertEquals(127, $values[8]['value']);
+
+        // Double
+        $this->assertEquals(6, $values[10]['type']);
+        $this->assertEquals('doubleTest', $values[10]['name']);
+        $this->assertEquals(0.49312871321823, $values[10]['value']);
+    }
+
+    /**
+     * @depends testLoadSmallNBT
+     * @depends testLoadBigNBT
+     */
+    public function testWriteFile() {
+        $this->nbt->loadFile('smalltest.nbt');
+        $this->nbt->loadFile('bigtest.nbt');
+        $this->nbt->writeFile($tmp = tempnam(sys_get_temp_dir(), 'nbt'));
+
+        $this->assertFileExists($tmp);
+    }
+
+}
